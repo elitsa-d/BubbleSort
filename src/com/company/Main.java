0801f4aad53aff1b40cee8d364a70d4a010d94ef
Elitsa Dimitrova <elitsa.v.dimitrova@protonmail.com>
2023-11-03 12:04:48 +0200

    Implement selection sort

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,18 @@ public class Main {
     public static void main(String[] args) {
         int[] array = {12, 2, 43, 1, 7, 54, 0};
         BubbleSort.sort(array);
+        Main.printArray(array);
 
+        int[] array1 = {132, 13, 65, 7, 1, 101, 40};
+        SelectionSort.sort(array1);
+        Main.printArray(array1);
+    }
+
+    public static void printArray(int[] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
+
+        System.out.println();
     }
 }
--- src/com/company/SelectionSort.java
+++ src/com/company/SelectionSort.java
@@ -1,5 +1,21 @@
 package com.company;
 
 public class SelectionSort {
+    public static void sort(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            int minIndex = i;
 
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            if (minIndex != i) {
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
+        }
+    }
 }
