06589b1ea46b22ac594737d82c6bebcfd96e5ced
Elitsa Dimitrova <elitsa.v.dimitrova@protonmail.com>
2024-01-10 14:11:19 +0200

    Add comment about insertion sort complexity

--- src/com/company/InsertionSort.java
+++ src/com/company/InsertionSort.java
@@ -1,16 +1,20 @@
 package com.company;
 
+
+// Time complexity: O(n^2)
+// Space complexity: O(1)
+
 public class InsertionSort {
     public static void sort(int[] array) {
         for (int i = 1; i < array.length; i++) {
-            int current = array[i];
+            int currentNum = array[i];
             int j = i;
-            while (j > 0 && array[j - 1] > current) {
+            while (j > 0 && array[j - 1] > currentNum) {
                 array[j] = array[j - 1];
                 j--;
             }
 
-            array[j] = current;
+            array[j] = currentNum;
         }
     }
 }
