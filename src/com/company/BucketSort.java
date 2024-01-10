3dd2a452b65ad9a1c768a92c6b775b0c0f1c0dd7
Elitsa Dimitrova <elitsa.v.dimitrova@protonmail.com>
2024-01-10 15:52:21 +0200

    Add comment about bucket sort complexity

--- src/com/company/BucketSort.java
+++ src/com/company/BucketSort.java
@@ -3,6 +3,9 @@ package com.company;
 import java.util.ArrayList;
 import java.util.Collections;
 
+// Time complexity: O(nlogn)
+// Space complexity: O(n)
+
 public class BucketSort {
     public static void sort(int[] array) {
          int numberOfBuckets = (int) Math.ceil(Math.sqrt(array.length));
